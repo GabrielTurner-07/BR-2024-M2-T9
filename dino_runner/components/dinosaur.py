bcb428f1c87303e0dbbd304d720d192183211161
GabrielTurner-07 <61908707+GabrielTurner-07@users.noreply.github.com>
2023-10-19 20:42:10 -0300

    Add files via upload

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,8 +1,9 @@
 import pygame
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
+Y_DUCK_POS = Y_POS + 25
 JUMP_VEL = 8.5
 
 class Dinosaur:
@@ -15,12 +16,15 @@ class Dinosaur:
         self.jump_vel = JUMP_VEL
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
 
     def update(self, user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
-            self.jump()  
+            self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
@@ -28,6 +32,14 @@ class Dinosaur:
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = False
+            self.dino_duck = True
+        elif not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = True
+            self.dino_duck = False
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -51,7 +63,14 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL  
 
     def duck(self):
-        pass                  
+        def duck(self):
+            self.image = DUCKING[self.type][self.step_index // 5]
+            self.image = pygame.transform.scale(self.image, (150, 70))
+            self.dino_rect = self.image.get_rect()
+            self.dino_rect.x = X_POS
+            self.dino_rect.y = Y_DUCK_POS
+            self.step_index += 1
+            self.dino_duck = False
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
