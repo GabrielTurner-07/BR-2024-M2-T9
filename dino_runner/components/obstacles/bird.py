7175363313c48e582203de3fff62c1c29de4130c
GabrielTurner-07 <61908707+GabrielTurner-07@users.noreply.github.com>
2023-10-19 22:02:15 -0300

    TAREFA: BIRD, LARGE_CACTUS

---
+++ dino_runner/components/obstacles/bird.py
@@ -0,0 +1,18 @@
+from dino_runner.utils.constants import BIRD
+from dino_runner.components.obstacles.obstacle import Obstacle
+import random
+
+BIRD_HEIGHTS = [425, 465, 489]
+
+class Bird(Obstacle):
+    def __init__(self):
+        super().__init__(BIRD, 0)
+        self.rect.y = random.choice(BIRD_HEIGHTS)
+        self.step_index = 0
+
+    def draw(self, screen):
+        screen.blit(self.image[self.step_index // 5], self.rect)
+        self.step_index += 1
+
+        if self.step_index >= 10:
+            self.step_index = 0
